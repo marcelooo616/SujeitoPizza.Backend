685bec4b86f535973b7d5c9f0550adf21f4bf6da
marcelooo616 <marcelooo2014@hotmail.com>
2023-01-21 16:36:04 -0300

    :sparkles: feat: Listando categorias

---
+++ src/services/category/ListCategoryService.ts
@@ -0,0 +1,19 @@
+import { prisma } from "@prisma/client";
+import prismaClient from "../../prisma/Index";
+
+class ListCategoryService{
+    async execute(){
+
+        const category =  await prismaClient.category.findMany({
+            select:{
+                id: true,
+                name: true
+            }
+        })
+
+        return category
+
+    }
+}
+
+export { ListCategoryService}
