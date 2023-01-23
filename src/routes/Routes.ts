2c39fe4960a26bdf07f859cae0ddaa49c6b91bb5
marcelooo616 <marcelooo2014@hotmail.com>
2023-01-23 16:56:27 -0300

    :sparkles: feat: Fechando um pedido

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -15,6 +15,7 @@ import { ListByCategoryController } from '../controllers/product/ListByCategoryC
 
 import uploadConfig from '../config/multer'
 import { CreateOrderController } from '../controllers/order/CreateOrderController';
+import { RemoveOrderController } from '../controllers/order/RemoveOrderController';
 
 const router = Router();
 
@@ -38,6 +39,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 // -- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 
 export { router }; 
