fbcfcd58c277f149302e346463f82df18d2c6b4f
marcelooo616 <marcelooo2014@hotmail.com>
2023-01-25 16:15:43 -0300

    :sparkles: feat: Retirando itens da mesa

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -17,6 +17,7 @@ import uploadConfig from '../config/multer'
 import { CreateOrderController } from '../controllers/order/CreateOrderController';
 import { RemoveOrderController } from '../controllers/order/RemoveOrderController';
 import { AddItemController } from '../controllers/order/AddItemController';
+import { RemoveItemController } from '../controllers/order/RemoveItemController';
 
 const router = Router();
 
@@ -42,6 +43,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 router.post('/order/add', isAuthenticated, new  AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 
 
 export { router }; 
